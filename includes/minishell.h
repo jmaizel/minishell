f17b278ddd3465c892a01084164f8265b91de521
Jacob Maizel <jmaizel@fu-r8-p6.s19.be>
2025-01-15 17:10:19 +0100

    minishell

--- includes/minishell.h
+++ includes/minishell.h
@@ -12,6 +12,55 @@
 # include <limits.h>
 # include <errno.h>
 # include <signal.h>
+#include <stdbool.h>
+
+typedef enum e_token_type
+{
+    TOK_COMMAND,      // Représente une commande simple
+    TOK_ARGUMENT,     // Représente un argument
+    TOK_PIPE,         // Représente un pipe '|'
+    TOK_REDIRECTION,  // Représente une redirection, comme '>' ou '<'
+    TOK_PIPE_OUT,     // Représente un pipe sortant
+    TOK_PIPE_IN,      // Représente un pipe entrant
+    TOK_EOF,          // Fin de fichier
+    TOK_INVALID       // Token invalide
+} t_token_type;
+
+typedef struct s_tokens
+{
+    t_token_type type;   // Le type du token (par exemple, commande, argument, pipe, etc.)
+    char *value;         // La valeur littérale du token (par exemple, le texte de la commande ou de l'argument)
+    struct s_tokens *next;  // Pointeur vers le token suivant
+} t_tokens;
+
+typedef struct s_tools
+{
+    char **env;            // Variables d'environnement (tableau de chaînes de caractères)
+    int exit_code;         // Code de sortie de la dernière commande exécutée
+    t_simple_cmds *cmds;   // Liste des commandes à exécuter
+    t_tokens *tokens;      // Liste des tokens générés par le lexer
+    // D'autres champs peuvent être ajoutés ici selon les besoins du projet
+} t_tools;
+
+typedef struct s_lexer
+{
+	char    	*str;
+    t_tokens        token;
+	int		i;
+	struct s_lexer	*next;
+	struct s_lexer	*prev;
+}	t_lexer;
+
+typedef struct s_simple_cmds
+{
+	char                    **str;
+	int                     (*builtin)(t_tools *, struct s_simple_cmds *);
+	int                     num_redirections;
+	char                    *hd_file_name;
+	t_lexer                 *redirections;
+	struct s_simple_cmds	*next;
+	struct s_simple_cmds	*prev;
+}	t_simple_cmds;
 
 //builtins
 
--- srcs/parsing/parsing1.c
+++ srcs/parsing/parsing1.c
@@ -6,28 +6,62 @@
 /*   By: jmaizel <jmaizel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 12:19:12 by jmaizel           #+#    #+#             */
-/*   Updated: 2025/01/15 12:33:18 by jmaizel          ###   ########.fr       */
+/*   Updated: 2025/01/15 14:23:22 by jmaizel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// cette fonction : on prend en param dq et sq pour voir si on est dans une " et
+// sa pour savoir si on est dans une '.
+// explication de l algo , on regarde si cest un /' ou un " et si sq et dq sont false, cad qu aucune des deux possibilite n est ouverte. 
+// si c est
+
+void	quoting_choice(bool *dq, bool *sq, int *index, char c)
+{
+	if ((c == '\'' || c == '"') && !*sq && !*dq)
+	{
+		if (c == '\'' && !*dq)
+			*sq = true;
+		else if (c == '"' && !*sq)
+			*dq = true;
+		if (index)
+			++(*index);
+	}
+	else if ((c == '\'' || c == '"'))
+	{
+		if (c == '\'' && !*dq && *sq)
+			*sq = false;
+		else if (c == '"' && !*sq && *dq)
+			*dq = false;
+		if (index)
+			++(*index);
+	}
+}
+
 int	is_quote_closed(char *str)
 {
-	int	single_quote;
-	int	double_quote;
-	int	i;
+	bool	dq;
+	bool	sq;
+	int		i;
 
-	single_quote = 0;
-	double_quote = 0;
+	dq = false;
+	sq = false;
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == '\'' && double_quote == 0)
-			single_quote = !single_quote;
-		else if (str[i] == '\"' && single_quote == 0)
-			double_quote = !double_quote;
+		if (str[i] == '\'' && dq == false)
+		{
+			sq = !sq;
+		}
+		else if (str[i] == '\"' && sq == false)
+		{
+			dq = !dq;
+		}
 		i++;
 	}
-	return (single_quote == 0 && double_quote == 0);
+	if (dq || sq)
+		return (0);
+	else
+		return (1);
 }
