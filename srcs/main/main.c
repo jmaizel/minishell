3e8dce83a4f62b4bfc59c6137dc4984052f69eae
Dededessuslesmoustier Clément <clement.dedessuslesmous@student.vinci.be>
2025-01-17 14:25:40 +0100

    refactor - main.c

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -6,52 +6,41 @@
 /*   By: cdedessu <cdedessu@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 12:20:12 by jmaizel           #+#    #+#             */
-/*   Updated: 2025/01/17 14:06:38 by cdedessu         ###   ########.fr       */
+/*   Updated: 2025/01/17 14:24:34 by cdedessu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "minishell.h"
 
 #include "../includes/execution.h"
 
 int	main(int argc, char **argv, char **envp)
 {
-	char		*line;
-	t_tools		tools;
+	char			*line;
+	t_tools			tools;
 	t_simple_cmds	cmd;
 
 	(void)argc;
 	(void)argv;
-
-	// Initialisation des outils
 	tools.env = envp;
 	tools.exit_code = 0;
 
-	// Boucle principale du shell
 	while (1)
 	{
-		// Afficher le prompt et lire la commande utilisateur
 		line = readline("minishell> ");
-		if (!line) // Gestion de Ctrl-D
+		if (!line)
 		{
 			ft_putendl_fd("exit", STDOUT_FILENO);
 			break ;
 		}
-		add_history(line); // Ajouter la commande à l'historique
-		
-		// Parsing de la commande
-		// Ici, vous pouvez appeler une fonction `parse_input` pour analyser `line`
-		// Exemple d'initialisation manuelle (temporaire) :
+		add_history(line);
+	
 		cmd.str = (char *[]){line, NULL};
-		cmd.redirections = NULL; // À configurer si nécessaire
+		cmd.redirections = NULL;
 		cmd.next = NULL;
 		cmd.prev = NULL;
 
-		// Exécution de la commande
-		apply_redirections(&cmd); // Gérer les redirections (temporairement vide si pas de redirections)
+		apply_redirections(&cmd); 
 		execute_simple_command(&cmd, &tools);
 
-		// Nettoyage
 		free(line);
 	}
 	return (tools.exit_code);
