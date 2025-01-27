11b171b6384f085d60b50059b24b19a653b7e044
Clement Dedessuslesmoustier <cdedessu@fu-r7-p8.s19.be>
2025-01-27 16:58:04 +0100

    refactor - norminette for exec files BUT NOT FOR 25 LINES

--- includes/execution.h
+++ includes/execution.h
@@ -6,7 +6,7 @@
 /*   By: cdedessu <cdedessu@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 10:31:48 by cdedessu          #+#    #+#             */
-/*   Updated: 2025/01/27 15:54:23 by cdedessu         ###   ########.fr       */
+/*   Updated: 2025/01/27 16:57:35 by cdedessu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,6 @@
 # include <stdbool.h>
 # include "builtins.h"
 
-/* Macros et codes de retour */
 # define ERROR_EXIT 1
 # define SUCCESS_EXIT 0
 # define CMD_NOT_FOUND 127
@@ -38,47 +37,36 @@
 # define GENERAL_ERROR 1
 
 /* Exécution des commandes */
-void    execute_simple_command(t_simple_cmds *cmd, t_tools *tools);
-void    execute_pipeline(t_simple_cmds *cmd, t_tools *tools);
-void    update_exit_status(t_tools *tools, int status);
-int     get_command_exit_status(t_simple_cmds *cmd);
+void	execute_simple_command(t_simple_cmds *cmd, t_tools *tools);
+void	execute_pipeline(t_simple_cmds *cmd, t_tools *tools);
+void	update_exit_status(t_tools *tools, int status);
+int		get_command_exit_status(t_simple_cmds *cmd);
 
 /* Gestion des redirections */
-void    apply_redirections(t_simple_cmds *cmd);
-void    handle_input_redirection(t_lexer *redir);
-void    handle_output_redirection(t_lexer *redir);
-void    handle_append_redirection(t_lexer *redir);
-void    handle_heredoc(t_lexer *redir);
+void	apply_redirections(t_simple_cmds *cmd);
+void	handle_input_redirection(t_lexer *redir);
+void	handle_output_redirection(t_lexer *redir);
+void	handle_append_redirection(t_lexer *redir);
+void	handle_heredoc(t_lexer *redir);
 
 /* Gestion des variables d'environnement */
-char    *get_env_var(char *key, char **env);
-int     add_env_var(char *var, char ***env);
-int     remove_env_var(char *key, char ***env);
-char    *expand_variables(char *str, t_tools *tools);
+char	*get_env_var(char *key, char **env);
+int		add_env_var(char *var, char ***env);
+int		remove_env_var(char *key, char ***env);
+char	*expand_variables(char *str, t_tools *tools);
 
 /* Utilitaires */
-char    *find_executable(char *command, char **env);
-int     ft_strcmp(const char *s1, const char *s2);
-void    handle_error(const char *msg);
-int     cleanup_pipes(int prev_pipe, int pipe_fds[2], bool has_next);
-void    cleanup_command(t_simple_cmds *cmd);
-char    *ft_charjoin(char *str, char c);
+char	*find_executable(char *command, char **env);
+int		ft_strcmp(const char *s1, const char *s2);
+void	handle_error(const char *msg);
+int		cleanup_pipes(int prev_pipe, int pipe_fds[2], bool has_next);
+void	cleanup_command(t_simple_cmds *cmd);
+char	*ft_charjoin(char *str, char c);
 
 /* Gestion des signaux */
-void    sigint_handler(int sig);
-void    child_sigint_handler(int sig);
-void    setup_signals(void);
-void    setup_child_signals(void);
-
-/* Builtins */
-int     builtin_echo(t_simple_cmds *cmd);
-int     builtin_cd(t_simple_cmds *cmd, t_tools *tools);
-int     builtin_pwd(void);
-int     builtin_export(t_simple_cmds *cmd, t_tools *tools);
-int     builtin_unset(t_simple_cmds *cmd, t_tools *tools);
-int     builtin_env(t_tools *tools);
-int     builtin_exit(t_simple_cmds *cmd);
-
-
+void	sigint_handler(int sig);
+void	child_sigint_handler(int sig);
+void	setup_signals(void);
+void	setup_child_signals(void);
 
 #endif
