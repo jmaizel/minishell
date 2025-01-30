2727187c3f057e25f74ea73b53673971941d3c75
Clement Dedessuslesmoustier <cdedessu@fu-r7-p8.s19.be>
2025-01-30 16:52:40 +0100

    refactor - exit builtin NORMINETTE OK

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,62 +6,55 @@
 /*   By: cdedessu <cdedessu@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 14:11:03 by cdedessu          #+#    #+#             */
-/*   Updated: 2025/01/30 16:06:02 by cdedessu         ###   ########.fr       */
+/*   Updated: 2025/01/30 16:51:03 by cdedessu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/builtins.h"
 
-static int      is_numeric(const char *str)
+static int	is_numeric(const char *str)
 {
-        int     i;
+	int	i;
 
-        if (!str)
-                return (0);
-        i = 0;
-        if (str[i] == '-' || str[i] == '+')
-                i++;
-        while (str[i])
-        {
-                if (!ft_isdigit(str[i]))
-                        return (0);
-                i++;
-        }
-        return (1);
+	if (!str)
+		return (0);
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int             builtin_exit(t_simple_cmds *cmd, t_tools *tools)
+int	builtin_exit(t_simple_cmds *cmd, t_tools *tools)
 {
-        int     exit_code;
+	int	exit_code;
 
-        ft_putendl_fd("exit", STDOUT_FILENO);
-        if (!cmd || !cmd->str || !tools)
-                return (1);
-
-        if (cmd->str[1] && cmd->str[2])
-        {
-                ft_putendl_fd("minishell: exit: too many arguments", 
-                        STDERR_FILENO);
-                return (1);
-        }
-
-        if (cmd->str[1])
-        {
-                if (!is_numeric(cmd->str[1]))
-                {
-                        ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
-                        ft_putstr_fd(cmd->str[1], STDERR_FILENO);
-                        ft_putendl_fd(": numeric argument required", 
-                                STDERR_FILENO);
-                        exit_code = 2;
-                }
-                else
-                        exit_code = ft_atoi(cmd->str[1]);
-        }
-        else
-                exit_code = tools->exit_code;
-
-        // Ne pas libérer tools ici - c'est la responsabilité du main
-        exit(exit_code % 256);
-        return (exit_code);  // Ne sera jamais atteint à cause de exit()
+	ft_putendl_fd("exit", STDOUT_FILENO);
+	if (!cmd || !cmd->str || !tools)
+		return (1);
+	if (cmd->str[1] && cmd->str[2])
+	{
+		ft_putendl_fd("minishell: exit: too many arguments", STDERR_FILENO);
+		return (1);
+	}
+	if (cmd->str[1])
+	{
+		if (!is_numeric(cmd->str[1]))
+		{
+			ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+			ft_putstr_fd(cmd->str[1], STDERR_FILENO);
+			ft_putendl_fd(": numeric argument required", STDERR_FILENO);
+			exit_code = 2;
+		}
+		else
+			exit_code = ft_atoi(cmd->str[1]);
+	}
+	else
+		exit_code = tools->exit_code;
+	exit(exit_code % 256);
 }
