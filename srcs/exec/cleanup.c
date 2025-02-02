2c2ef18a197a27732fa631ad29c4ffc32c72c9b2
Dededessuslesmoustier Clément <clement.dedessuslesmous@student.vinci.be>
2025-02-02 19:45:55 +0100

    refactor - unset and cleanup NORMINETTE OK

--- srcs/exec/cleanup.c
+++ srcs/exec/cleanup.c
@@ -6,7 +6,7 @@
 /*   By: cdedessu <cdedessu@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 11:03:16 by cdedessu          #+#    #+#             */
-/*   Updated: 2025/01/31 16:33:40 by cdedessu         ###   ########.fr       */
+/*   Updated: 2025/02/02 19:45:22 by cdedessu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,15 +33,9 @@ void	cleanup_parsed_cmd(t_parsed_cmd *cmd)
 	if (!cmd)
 		return ;
 	if (cmd->full_cmd)
-	{
 		free(cmd->full_cmd);
-		cmd->full_cmd = NULL;
-	}
 	if (cmd->cmd)
-	{
 		free(cmd->cmd);
-		cmd->cmd = NULL;
-	}
 	free_str_array(cmd->input_file);
 	free_str_array(cmd->output_file);
 	free_str_array(cmd->append_file);
@@ -59,10 +53,7 @@ void	cleanup_pip(t_pip *pip)
 	{
 		next = current->next;
 		if (current->cmd_pipe)
-		{
 			free(current->cmd_pipe);
-			current->cmd_pipe = NULL;
-		}
 		if (current->redirection)
 			cleanup_parsed_cmd(current->redirection);
 		free(current);
@@ -75,14 +66,8 @@ void	cleanup_cmd_args(t_cmd_args *args)
 	if (!args)
 		return ;
 	if (args->argv)
-	{
 		free_str_array(args->argv);
-		args->argv = NULL;
-	}
 	if (args->cmd)
-	{
 		free(args->cmd);
-		args->cmd = NULL;
-	}
 	free(args);
 }
