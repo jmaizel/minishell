ed91818520d4a7ee0ad7a230dc35c93e21b262f0
Dededessuslesmoustier Clément <clement.dedessuslesmous@student.vinci.be>
2025-02-26 20:34:44 +0100

    init branch NOT WORKING

--- srcs/execution/exec_redir.c
+++ srcs/execution/exec_redir.c
@@ -6,22 +6,29 @@
 /*   By: cdedessu <cdedessu@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 17:43:39 by cdedessu          #+#    #+#             */
-/*   Updated: 2025/02/26 19:55:17 by cdedessu         ###   ########.fr       */
+/*   Updated: 2025/02/26 20:14:19 by cdedessu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/execution.h"
 
-static int  handle_output_redir(char *file, int append)
+static int handle_output_redir(char *file, int append)
 {
-    int     fd;
-    int     flags;
+    int fd;
+    int flags;
+
+    if (!file)
+    {
+        ft_printf("minishell: redirection error: no file specified\n");
+        return (-1);
+    }
 
     flags = O_WRONLY | O_CREAT;
     if (append)
         flags |= O_APPEND;
     else
         flags |= O_TRUNC;
+
     fd = open(file, flags, 0644);
     if (fd == -1)
     {
@@ -37,41 +44,41 @@ static int  handle_output_redir(char *file, int append)
     return (0);
 }
 
-static int  setup_input(t_parsed_cmd *cmd, t_process *process, t_exec *exec)
+static int setup_input(t_parsed_cmd *cmd, t_process *process, t_exec *exec)
 {
-    int fd;
+	int fd;
 
-    if (cmd->heredoc_count > 0)
-    {
-        fd = handle_heredoc(cmd, exec);
-        if (fd == -1)
-            return (-1);
-        process->stdin_backup = dup(STDIN_FILENO);
-        if (dup2(fd, STDIN_FILENO) == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-        close(fd);
-    }
-    else if (cmd->input_count > 0)
-    {
-        fd = open(cmd->input_file[cmd->input_count - 1], O_RDONLY);
-        if (fd == -1)
-        {
-            ft_printf("minishell: %s: No such file or directory\n",
-                cmd->input_file[cmd->input_count - 1]);
-            return (-1);
-        }
-        process->stdin_backup = dup(STDIN_FILENO);
-        if (dup2(fd, STDIN_FILENO) == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-        close(fd);
-    }
-    return (0);
+	if (cmd->heredoc_count > 0)
+	{
+		fd = handle_heredoc(cmd, exec);
+		if (fd == -1)
+			return (-1);
+		process->stdin_backup = dup(STDIN_FILENO);
+		if (dup2(fd, STDIN_FILENO) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		close(fd);
+	}
+	else if (cmd->input_count > 0)
+	{
+		fd = open(cmd->input_file[cmd->input_count - 1], O_RDONLY);
+		if (fd == -1)
+		{
+			ft_printf("minishell: %s: No such file or directory\n",
+					  cmd->input_file[cmd->input_count - 1]);
+			return (-1);
+		}
+		process->stdin_backup = dup(STDIN_FILENO);
+		if (dup2(fd, STDIN_FILENO) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		close(fd);
+	}
+	return (0);
 }
 
 int setup_redirections(t_parsed_cmd *cmd, t_process *process, t_exec *exec)
@@ -81,6 +88,7 @@ int setup_redirections(t_parsed_cmd *cmd, t_process *process, t_exec *exec)
 
     if (!cmd)
         return (0);
+
     process->stdin_backup = -1;
     process->stdout_backup = -1;
 
@@ -90,11 +98,14 @@ int setup_redirections(t_parsed_cmd *cmd, t_process *process, t_exec *exec)
     if (cmd->output_count > 0)
     {
         process->stdout_backup = dup(STDOUT_FILENO);
+        if (process->stdout_backup == -1)
+        {
+            ft_printf("minishell: dup error\n");
+            return (-1);
+        }
 
-        ft_printf("DEBUG: output_count = %d\n", cmd->output_count);
         for (i = 0; i < cmd->output_count - 1; i++)
         {
-            ft_printf("DEBUG: Opening intermediate file: %s\n", cmd->output_file[i]);
             if (!cmd->output_file[i])
             {
                 ft_printf("minishell: invalid output file at index %d\n", i);
@@ -109,7 +120,6 @@ int setup_redirections(t_parsed_cmd *cmd, t_process *process, t_exec *exec)
             close(fd);
         }
 
-        ft_printf("DEBUG: Redirecting to final file: %s\n", cmd->output_file[cmd->output_count - 1]);
         if (!cmd->output_file[cmd->output_count - 1])
         {
             ft_printf("minishell: invalid final output file\n");
@@ -118,20 +128,23 @@ int setup_redirections(t_parsed_cmd *cmd, t_process *process, t_exec *exec)
         if (handle_output_redir(cmd->output_file[cmd->output_count - 1], 0) == -1)
             return (-1);
     }
+
     return (0);
 }
 
-void    restore_redirections(t_process *process)
+void restore_redirections(t_process *process)
 {
     if (process->stdin_backup != -1)
     {
-        dup2(process->stdin_backup, STDIN_FILENO);
+        if (dup2(process->stdin_backup, STDIN_FILENO) == -1)
+            ft_printf("minishell: error restoring stdin\n");
         close(process->stdin_backup);
         process->stdin_backup = -1;
     }
     if (process->stdout_backup != -1)
     {
-        dup2(process->stdout_backup, STDOUT_FILENO);
+        if (dup2(process->stdout_backup, STDOUT_FILENO) == -1)
+            ft_printf("minishell: error restoring stdout\n");
         close(process->stdout_backup);
         process->stdout_backup = -1;
     }
--- srcs/main/main.c
+++ srcs/main/main.c
@@ -6,134 +6,75 @@
 /*   By: cdedessu <cdedessu@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 17:44:53 by cdedessu          #+#    #+#             */
-/*   Updated: 2025/02/26 19:42:54 by cdedessu         ###   ########.fr       */
+/*   Updated: 2025/02/26 20:33:50 by cdedessu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 #include "../../includes/execution.h"
 
-
-int g_signal_received = 0;
-
-// Fonction pour dupliquer l’environnement
-static char **dup_env(char **env)
+static void	init_minishell(t_tools *tools, char **env)
 {
-    int count;
-    char **new_env;
-    int i;
-
-    count = 0;
-    while (env && env[count])
-        count++;
-    new_env = malloc(sizeof(char *) * (count + 1));
-    if (!new_env)
-        return (NULL);
-    i = 0;
-    while (i < count)
-    {
-        new_env[i] = ft_strdup(env[i]);
-        if (!new_env[i])
-        {
-            while (i > 0)
-                free(new_env[--i]);
-            free(new_env);
-            return (NULL);
-        }
-        i++;
-    }
-    new_env[count] = NULL;
-    return (new_env);
+	ft_memset(tools, 0, sizeof(t_tools));
+	tools->env = env;
+	tools->exit_code = 0;
+	tools->tokens = NULL;
+	tools->cmds = NULL;
 }
 
-static void init_minishell(t_tools *tools, char **env)
+void	cleanup_minishell(t_tools *tools)
 {
-    ft_memset(tools, 0, sizeof(t_tools));
-    tools->env = dup_env(env); // Copie dynamique de l’environnement
-    if (!tools->env)
-    {
-        ft_putstr_fd("minishell: failed to initialize environment\n", 2);
-        exit(1);
-    }
-    tools->exit_code = 0;
-    tools->tokens = NULL;
-    tools->cmds = NULL;
+	(void)tools;
+	rl_clear_history();
 }
 
-void cleanup_minishell(t_tools *tools)
+static int	handle_execution(t_tools *tools, char *user_input)
 {
-    int i;
+	t_sep	*cell;
 
-    // Libère tools->env
-    if (tools->env)
-    {
-        i = 0;
-        while (tools->env[i])
-        {
-            free(tools->env[i]);
-            i++;
-        }
-        free(tools->env);
-    }
-    rl_clear_history();
+	if (!user_input || check_invalid_chars(user_input))
+	{
+		ft_printf("Error: Invalid input\n");
+		return (1);
+	}
+	cell = create_cell(ft_strdup(user_input));
+	if (!cell)
+		return (1);
+	parse_pipes(cell);
+	if (cell && cell->pipcell)
+	{
+		setup_exec_signals();
+		parsing_line(user_input, tools);
+		exec_commands(cell, tools);
+		restore_signals();
+	}
+	free_cell(cell);
+	return (0);
 }
 
-static int handle_execution(t_tools *tools, char *user_input)
+int	main(int argc, char **argv, char **env)
 {
-    t_sep *cell;
+	t_tools	tools;
+	char	*user_input;
 
-    if (!user_input)
-        return (1);
-    // Vérifie uniquement un pipe initial
-    if (user_input[0] == '|')
-    {
-        ft_printf("minishell: syntax error near unexpected token '|'\n");
-        return (1);
-    }
-    // Garde une vérification minimale pour les caractères invalides
-    if (check_invalid_chars(user_input))
-    {
-        ft_printf("minishell: syntax error: invalid characters\n");
-        return (1);
-    }
-    cell = create_cell(ft_strdup(user_input));
-    if (!cell)
-        return (1);
-    parse_pipes(cell);
-    if (cell && cell->pipcell)
-    {
-        setup_exec_signals();
-        parsing_line(user_input, tools);
-        exec_commands(cell, tools);
-        restore_signals();
-    }
-    free_cell(cell);
-    return (0);
-}
-
-int main(int argc, char **argv, char **env)
-{
-    t_tools tools;
-    char *user_input;
-
-    (void)argc;
-    (void)argv;
-    init_minishell(&tools, env);
-    setup_interactive_signals();
-    while (1)
-    {
-        g_signal_received = 0;
-        user_input = get_user_input();
-        if (!user_input || !ft_strncmp(user_input, "exit", 5))
-        {
-            if (user_input)
-                free(user_input);
-            break;
-        }
-        if (user_input[0] != '\0')
-            handle_execution(&tools, user_input);
-        free(user_input);
-    }
-    cleanup_minishell(&tools);
-    return (tools.exit_code);
+	(void)argc;
+	(void)argv;
+	init_minishell(&tools, env);
+	setup_interactive_signals();
+	while (1)
+	{
+		g_signal_received = 0;
+		user_input = get_user_input();
+		if (!user_input || !ft_strncmp(user_input, "exit", 5))
+		{
+			if (user_input)
+				free(user_input);
+			break ;
+		}
+		if (user_input[0] != '\0')
+			handle_execution(&tools, user_input);
+		free(user_input);
+	}
+	cleanup_minishell(&tools);
+	return (tools.exit_code);
 }
