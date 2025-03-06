1813deff06a0f36e3ae67744415f02f002a8e15e
jmaizel <jacobmaizel@hotmail.com>
2025-03-06 18:17:56 +0100

    Update unset.c

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: jmaizel <jmaizel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 20:18:28 by cdedessu          #+#    #+#             */
-/*   Updated: 2025/03/05 16:13:39 by jmaizel          ###   ########.fr       */
+/*   Updated: 2025/03/06 18:16:09 by jmaizel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,31 @@ static void	remove_env_var(char **env, int index)
 	env[index] = NULL;
 }
 
+static void	remove_from_export_vars(t_tools *tools, char *var_name)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < tools->export_count)
+	{
+		if (ft_strcmp(tools->export_vars[i], var_name) == 0)
+		{
+			free(tools->export_vars[i]);
+			j = i;
+			while (j < tools->export_count - 1)
+			{
+				tools->export_vars[j] = tools->export_vars[j + 1];
+				j++;
+			}
+			tools->export_vars[tools->export_count - 1] = NULL;
+			tools->export_count--;
+			return ;
+		}
+		i++;
+	}
+}
+
 int	builtin_unset(t_tools *tools, char **argv)
 {
 	int	i;
@@ -58,6 +83,7 @@ int	builtin_unset(t_tools *tools, char **argv)
 			env_idx = find_env_var(tools->env, argv[i]);
 			if (env_idx >= 0)
 				remove_env_var(tools->env, env_idx);
+			remove_from_export_vars(tools, argv[i]);
 		}
 		i++;
 	}
